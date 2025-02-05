8ea87f12c06456db20c6cf9796f6f7dbc528946c
Qureshi Abdul Kader Javed <81419817+AbdulkaderJaved-SoftwareEngineer@users.noreply.github.com>
2025-02-05 18:08:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ model = tf.keras.models.load_model(output)
 class_names = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
 
 # Streamlit interface
-st.title("AbdulKader Javed Qureshi Potato Disease Detection")
+st.title("AbdulKader Javed Qureshi Potato Leaf Disease Detection")
+
 st.write("Upload an image of a potato leaf to detect the disease.")
 
 # Image upload
